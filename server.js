f3406870f2156a60e171a8fb948b7f7f7c195817
USER <jneralrex@gmail.com>
2026-07-15 12:02:39 +0100

    Chores: Added allowed origins for cors

--- server.js
+++ server.js
@@ -30,8 +30,16 @@ autoCancelUnpaidOrders();
 
 
 app.use(cors({ 
-    origin: "*",  
-    credentials: true 
+     origin: [
+    "http://localhost:5173", 
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176",
+    "https://sartorhealth-com.onrender.com"
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, 
 }));
 app.use(cookieParser());
 
