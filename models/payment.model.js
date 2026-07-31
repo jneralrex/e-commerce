66a1f345c0c4ac1a895244ffc44cffc9f960b4ba
USER <jneralrex@gmail.com>
2026-07-31 10:30:26 +0100

    updating model for payment in order to sort duplicate error

--- models/payment.model.js
+++ models/payment.model.js
@@ -7,79 +7,59 @@ const paymentSchema = new mongoose.Schema(
             ref: "Order",
             required: true
         },
-
         account: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             required: true
         },
-
         gateway: {
             type: String,
             enum: ["paystack"],
             default: "paystack"
         },
-
         payment_ref: {
             type: String,
             unique: true,
             required: true
-        }, // Internal reference
-
+        }, 
         paystack_reference: {
             type: String,
             unique: true,
-            sparse: true,
-            default: null
-        },  // Gateway reference
-
+            sparse: true
+        },  
         access_code: {
             type: String,
             default: null
-        }, // Paystack access code
-
+        }, 
         amount: {
             type: Number,
             required: true
         },
-
         currency: {
             type: String,
             enum: ["NGN", "USD"],
             required: true
         },
-
         status: {
             type: String,
-            enum: [
-                "PENDING",
-                "PROCESSING",
-                "PAID",
-                "FAILED",
-                "ABANDONED",
-                "REFUNDED"
-            ],
+            enum: ["PENDING", "PROCESSING", "PAID", "FAILED", "ABANDONED", "REFUNDED"],
             default: "PENDING"
         },
-
         gateway_response: {
             type: mongoose.Schema.Types.Mixed,
             default: null
         },
-
         attempt: {
             type: Number,
             default: 1,
         },
-
         gateway_transaction_id: {
             type: String,
         },
-
         authorization_url: {
             type: String,
             default: null
-        }, // Redirect URL
+        }, 
         metadata: {
             type: mongoose.Schema.Types.Mixed,
             default: {}
@@ -100,18 +80,12 @@ const paymentSchema = new mongoose.Schema(
     },
     {
         timestamps: true
-    });
-
-// paymentSchema.index({ payment_ref: 1 });
-
-// paymentSchema.index({ paystack_reference: 1 });
+    }
+);
 
 paymentSchema.index({ account: 1 });
-
 paymentSchema.index({ order: 1 });
-
 paymentSchema.index({ status: 1 });
-
 paymentSchema.index({ createdAt: -1 });
 
 module.exports = mongoose.model("Payment", paymentSchema);
