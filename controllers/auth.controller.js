44d2d5cd3f1686bf138abd73c85bda8540a38b77
USER <jneralrex@gmail.com>
2026-08-06 16:08:36 +0100

    corrected refresh token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -443,14 +443,11 @@ const signIn = async (req, res, next) => {
 
         await user.save();
 
-        const isProduction =
-            process.env.NODE_ENV === "production";
-
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: isProduction,
-            sameSite: "Strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         res.status(200).json({
